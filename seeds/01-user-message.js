0c2b9e9cd87066a4eb14013277d1c7eb842b1eda
Jay <jaykamas@gmail.com>
2020-11-09 16:06:57 -0500

    removed seeds

--- seeds/01-user-message.js
+++ seeds/01-user-message.js
@@ -4,61 +4,61 @@ exports.seed = async knex => {
   await knex('site').del()
   await knex('user').del()
 
-  await knex("user").insert([{
-    email: "jay.oliver.technologies@gmail.com"
-  },{
-    email: "jaykamas@gmail.com"
-  }
-  ])
+  // await knex("user").insert([{
+  //   email: "jay.oliver.technologies@gmail.com"
+  // },{
+  //   email: "jaykamas@gmail.com"
+  // }
+  // ])
 
-  await knex("site").insert([{
+  // await knex("site").insert([{
 
-    user_id: 1,
-    from_user: "jaykamas@gmail.com",
-    url: "https://en.wikipedia.org/wiki/Existentialism",
-    read: false
-  }])
+  //   user_id: 1,
+  //   from_user: "jaykamas@gmail.com",
+  //   url: "https://en.wikipedia.org/wiki/Existentialism",
+  //   read: false
+  // }])
 
-  await knex("highlight").insert([{
-    site_id: 1,
-    parentTag: "p",
-    text: "nquiry that explores the"
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "phasizing experie"
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "of the existentialist, the individual's starting point has been called \"the existential",
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "m is associated with several 19th- and 20th-cent"
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "who shared an emphasis on the human subj"
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "spite profound doctrinal differe",
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "tentialists regarded t",
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "and strongly influenced many d"
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "s one in accordance with one's freedom. A component of freedom is facticity, but not to the degree that this facticity d"
-  },{
-    site_id: 1,
-    parentTag: "p",
-    text: "bjective thought makes people forget they exist, and then went on to argue that it implies a claim that you do not",
-  }])
+  // await knex("highlight").insert([{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "nquiry that explores the"
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "phasizing experie"
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "of the existentialist, the individual's starting point has been called \"the existential",
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "m is associated with several 19th- and 20th-cent"
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "who shared an emphasis on the human subj"
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "spite profound doctrinal differe",
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "tentialists regarded t",
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "and strongly influenced many d"
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "s one in accordance with one's freedom. A component of freedom is facticity, but not to the degree that this facticity d"
+  // },{
+  //   site_id: 1,
+  //   parentTag: "p",
+  //   text: "bjective thought makes people forget they exist, and then went on to argue that it implies a claim that you do not",
+  // }])
 
 };
