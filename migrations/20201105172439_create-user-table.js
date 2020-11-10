ec74e6f806e408d301ebb760158b63e8a9806e3c
Jay <jaykamas@gmail.com>
2020-11-09 22:56:20 -0500

    added delete route

--- migrations/20201105172439_create-user-table.js
+++ migrations/20201105172439_create-user-table.js
@@ -1,21 +1,21 @@
 
 exports.up = async knex => {
     await knex.schema.createTable("user", table => {
-        table.increments()
+        table.increments().primary()
         table.string("email")
     })
 
     await knex.schema.createTable("site", table => {
-        table.increments()
-        table.integer("user_id").references("id").inTable("user")
+        table.increments().primary()
+        table.integer("user_id").references("id").inTable("user").onDelete('CASCADE')
         table.string("from_user")
         table.string("url")
         table.boolean("read")
     })
 
     await knex.schema.createTable("highlight", table => {
-        table.increments()
-        table.integer("site_id").references("id").inTable("site").onDelete('cascade')
+        table.increments().primary()
+        table.integer("site_id").references("id").inTable("site").onDelete('CASCADE')
         table.string("parentTag")
         table.string("text")
     })
